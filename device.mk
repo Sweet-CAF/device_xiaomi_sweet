8826071f7bd85e6b5ede5a71261250cc82c52d38
romiyusnandar <yusromi04@gmail.com>
2025-03-21 16:10:20 +0000

    sweet: Switch to QTI perf component
    Signed-off-by: romiyusnandar <yusromi04@gmail.com>

--- device.mk
+++ device.mk
@@ -388,15 +388,15 @@ PRODUCT_PACKAGES += \
 PRODUCT_BUILD_SUPER_PARTITION := false
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
-# Perf
-PRODUCT_PACKAGES += \
-    libqti-perfd-client
-
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/power/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
 # Platform
 TARGET_BOARD_PLATFORM := sm6150
+
+# QTI Component
+TARGET_COMMON_QTI_COMPONENTS := \
+    perf
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/permissions/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/qti_whitelist.xml \
