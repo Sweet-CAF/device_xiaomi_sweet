adf1e1730e947e012ee4d8cd77e0a4fd5af9cc6f
romiyusnandar <yusromi04@gmail.com>
2025-03-21 15:54:35 +0000

    sweet: Switch to power modes extension for battery saver
    Signed-off-by: romiyusnandar <yusromi04@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -195,6 +195,7 @@ TARGET_BOARD_PLATFORM := sm6150
 
 # Power
 TARGET_POWERHAL_MODE_EXT := $(DEVICE_PATH)/configs/power/power-mode.cpp
+TARGET_USES_INTERACTION_BOOST := true
 
 # Properties
 TARGET_ODM_PROP += $(DEVICE_PATH)/props/odm.prop
