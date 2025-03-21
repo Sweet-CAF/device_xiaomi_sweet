30d6b4ca4c7eb2f81e38deefee252a0e284e1349
romiyusnandar <yusromi04@gmail.com>
2025-03-21 17:05:17 +0000

    sweet: Switch to common QTI bt
    Signed-off-by: romiyusnandar <yusromi04@gmail.com>

--- device.mk
+++ device.mk
@@ -34,21 +34,12 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     fs_config_files
 
-# ANT+
-PRODUCT_PACKAGES += \
-    AntHalService-Soong \
-    com.dsi.ant@1.0.vendor
-
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@7.1-impl \
     android.hardware.audio.effect@7.0-impl \
     android.hardware.audio.service \
-    android.hardware.soundtrigger@2.3-impl \
-    android.hardware.bluetooth.audio@2.1-impl
-
-PRODUCT_PACKAGES += \
-    audio.bluetooth.default
+    android.hardware.soundtrigger@2.3-impl
 
 PRODUCT_PACKAGES += \
     libvolumelistener
@@ -84,23 +75,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
     frameworks/native/data/etc/android.hardware.audio.pro.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.pro.xml \
     frameworks/native/data/etc/android.software.midi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.midi.xml
-
-# Bluetooth
-PRODUCT_PACKAGES += \
-    libldacBT_enc \
-    libldacBT_abr \
-    android.hardware.bluetooth@1.0.vendor \
-    android.hardware.bluetooth@1.1.vendor \
-    vendor.qti.hardware.bluetooth_audio@2.1.vendor \
-    android.hardware.bluetooth.audio@2.0-impl \
-    audio.bluetooth.default \
-    vendor.qti.hardware.bluetooth_audio@2.1.vendor \
-    vendor.qti.hardware.btconfigstore@1.0.vendor \
-    vendor.qti.hardware.btconfigstore@2.0.vendor
-
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.bluetooth_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth_le.xml \
-    frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml
 
 # Camera
 PRODUCT_PACKAGES += \
@@ -352,6 +326,7 @@ TARGET_BOARD_PLATFORM := sm6150
 # QTI Component
 TARGET_COMMON_QTI_COMPONENTS := \
     audio \
+    bt \
     display \
     gps \
     perf
