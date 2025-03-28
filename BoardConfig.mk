59abbb12cd67589b330461ff03263d0146d81c5f
romiyusnandar <yusromi04@gmail.com>
2025-03-28 00:16:53 +0000

    sweet: Use sweet defconfig properly
    Signed-off-by: romiyusnandar <yusromi04@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -121,8 +121,7 @@ BOARD_KERNEL_IMAGE_NAME := Image
 TARGET_KERNEL_ADDITIONAL_FLAGS := AS=llvm-as AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip LD=ld.lld LLVM=1 LLVM_IAS=1
 KERNEL_SUPPORTS_LLVM_TOOLS := true
 TARGET_KERNEL_SOURCE := kernel/xiaomi/sweet
-TARGET_KERNEL_CONFIG := vendor/sdmsteppe-perf_defconfig
-KERNEL_FRAGMENT_CONFIG += vendor/sweet.config
+TARGET_KERNEL_CONFIG := vendor/sweet_user_defconfig
 BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200n8 earlycon=msm_geni_serial,0x880000
 BOARD_KERNEL_CMDLINE += androidboot.hardware=qcom androidboot.console=ttyMSM0
 BOARD_KERNEL_CMDLINE += androidboot.usbcontroller=a600000.dwc3
